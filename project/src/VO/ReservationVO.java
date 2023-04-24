ddaf2acbd9d4ce6d9751889e7e2ab5dd53c4da38
innateshy <innateshy@gmail.com>
2023-04-24 11:01:21 +0900

    merge error

--- project/src/VO/ReservationVO.java
+++ project/src/VO/ReservationVO.java
@@ -16,7 +16,46 @@ public class ReservationVO {
 		this.fk_routeid = fk_routeid;
 		
 	}
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBoardingdate() {
+		return boardingdate;
+	}
+
+	public void setBoardingdate(String boardingdate) {
+		this.boardingdate = boardingdate;
+	}
+
+	public int getSeatid() {
+		return seatid;
+	}
+
+	public void setSeatid(int seatid) {
+		this.seatid = seatid;
+	}
+
+	public int getFk_userid() {
+		return fk_userid;
+	}
+
+	public void setFk_userid(int fk_userid) {
+		this.fk_userid = fk_userid;
+	}
+
+	public int getFk_routeid() {
+		return fk_routeid;
+	}
+
+	public void setFk_routeid(int fk_routeid) {
+		this.fk_routeid = fk_routeid;
+	}
 	
 	
 }
